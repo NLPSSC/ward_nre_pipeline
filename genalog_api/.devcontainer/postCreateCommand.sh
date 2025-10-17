4ab1757614ef0ded48d297d9ef366d85b267bcc6
Dax Marek Westerman <11837526+dax-westerman@users.noreply.github.com>
2025-10-17 17:11:18 -0500

    wip

--- genalog_api/.devcontainer/postCreateCommand.sh
+++ genalog_api/.devcontainer/postCreateCommand.sh
@@ -2,3 +2,13 @@
 
 pip install --upgrade pip --root-user-action=ignore && \
     python -m pip install -r ./requirements.txt --root-user-action=ignore
+
+# pip install --upgrade pip --root-user-action=ignore && \
+#     python -m pip install -r ./requirements.txt --root-user-action=ignore
+
+# # Configure Git to use parent directory if .git exists there
+# if [ -d "../.git" ]; then
+#     git config --global --add safe.directory /workspace/..
+#     cd /workspace && git --git-dir=../.git --work-tree=.. status
+# fi
+
