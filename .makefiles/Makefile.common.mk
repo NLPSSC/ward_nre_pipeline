c01f9cff8e268828837fc143c10ed7359fa574b1
Dax Westerman <11837526+dax-westerman@users.noreply.github.com>
2025-10-18 12:08:11 -0500

    Simplified Makefiles

--- .makefiles/Makefile.common.mk
+++ .makefiles/Makefile.common.mk
@@ -44,3 +44,39 @@ endef
 
 endif	
 
+
+dev-%-build:
+	@docker compose -f .devcontainer/$*/docker-compose.yml build $*
+
+# Start (and attach to) the genalog_api service
+dev-%-up:
+	@docker compose -f .devcontainer/$*/docker-compose.yml up -d $*
+
+# Remove (stop and delete) the genalog_api container
+dev-%-down:
+	@docker compose -f .devcontainer/$*/docker-compose.yml down
+
+# Open a bash shell in the genalog_api container
+dev-%-shell: dev-$*-up
+	@docker compose -f .devcontainer/$*/docker-compose.yml exec $* /bin/bash
+
+# Run a command in the genalog_api container
+dev-%-exec:
+	@docker compose -f .devcontainer/$*/docker-compose.yml exec $* bash -c "$(cmd)"
+
+# Run tests in the genalog_api container
+dev-%-test:
+	@docker compose -f .devcontainer/$*/docker-compose.yml exec $* pytest
+
+
+help-%:
+	@echo "\033[1;34m$* Targets\033[0m"
+	@echo "----------------------------"
+	@echo ""
+	@echo "  \033[1;32mdev-$*-build\033[0m   Build the $* DevContainer image"
+	@echo "  \033[1;32mdev-$*-up\033[0m      Start (and attach to) the $* DevContainer"
+	@echo "  \033[1;32mdev-$*-shell\033[0m   Open a bash shell in the $* DevContainer"
+	@echo "  \033[1;32mdev-$*-exec\033[0m    Run a command in the $* DevContainer"
+	@echo "  \033[1;32mdev-$*-down\033[0m    Remove the $* DevContainer"
+	@echo "  \033[1;32mdev-$*-test\033[0m    Run tests in the $* DevContainer"
+	@echo ""
