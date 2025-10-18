da8467b1364f14f0cd684e5f9cb2f7d5e4207420
Dax Marek Westerman <11837526+dax-westerman@users.noreply.github.com>
2025-10-18 14:59:28 -0500

    wip

---
+++ .devcontainer/nre_pipeline/postCreateCommand.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+set -e
+echo "Running postCreateCommand script..."
+# Add any setup commands you want to run after the container is created here
+echo "Post-create setup complete."
