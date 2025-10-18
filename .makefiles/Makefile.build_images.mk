4ed0f778f30f5963c8b51dd4ce1fad7359b156d2
Dax Westerman <11837526+dax-westerman@users.noreply.github.com>
2025-10-18 14:04:29 -0500

    updated simplified Makefiles

--- .makefiles/Makefile.build_images.mk
+++ .makefiles/Makefile.build_images.mk
@@ -3,15 +3,8 @@ include .makefiles/Makefile.common.mk
 ifndef BUILD_IMAGES_INCLUDED
 BUILD_IMAGES_INCLUDED := 1
 
-.PHONY: help-build_images python38_base-build python38_base-rebuild python38_base-bash
+.PHONY: help-base-images
 
-help-build_images:
-	@echo "Base Image Targets:"
-	@echo ""
-	@echo "  \033[1;32mpython38_base-build\033[0m      Build the Python 3.8 base Docker image"
-	@echo "  \033[1;32mpython38_base-rebuild\033[0m    Rebuild the Python 3.8 base Docker image (no cache)"
-	@echo "  \033[1;32mpython38_base-bash\033[0m      Start a bash shell in the Python 3.8 base Docker image"
-	@echo ""
 
 ###############################################################################
 # Python 3.8 Base
@@ -22,125 +15,182 @@ PYTHON38_LABEL := python38_base
 PYTHON38_BASE_IMAGE_NAME := nlpssc/python38-base:latest
 PYTHON38_BASE_DOCKERFILE := .docker_imgs/$(PYTHON38_LABEL)/Dockerfile.python38_base
 
-python38_base-build:
-	@[ ! -f $(PYTHON38_BASE_DOCKERFILE) ] && \
-		echo "Dockerfile not found" || \
-		$(call docker-build, $(PYTHON38_BASE_DOCKERFILE), $(PYTHON38_BASE_IMAGE_NAME), .)
+get_label_text = $(shell .makefiles/get_config_value.sh $(1) 'text')
+get_label_image_name = $(shell .makefiles/get_config_value.sh $(1) 'image_name')
+get_label_dockerfile = $(shell .makefiles/get_config_value.sh $(1) 'dockerfile')
 
-python38_base-rebuild:
-	@$(MAKE) python38_base-build FORCE_REBUILD=1
+defined_base_images := python38_base python311_base python312_base medspacy_quickumls_processor
 
-python38_base-bash: python38_base-build
-	@docker run --rm -it --entrypoint /bin/bash $(PYTHON38_BASE_IMAGE_NAME)
+define show_error
+    printf "\033[1;31m%s\033[0m" $(1)
+endef
 
-python38_base-test: python38_base-build
-	@docker run --rm -it $(PYTHON38_BASE_IMAGE_NAME) /bin/bash -c "echo 'Testing Python 3.8 base image...'"
-
-python38_base-drop:
-	@if docker image inspect $(PYTHON38_BASE_IMAGE_NAME) > /dev/null 2>&1; then \
-		docker rmi -f $(PYTHON38_BASE_IMAGE_NAME); \
+validate-%:
+	@if [ "$(filter $*,$(defined_base_images))" = "" ]; then \
+		$(call show_error,"$* is not a valid base image target") \
+		exit 1; \
 	else \
-		echo "Image $(PYTHON38_BASE_IMAGE_NAME) does not exist."; \
+		echo "" && \
+			echo "Label: 		$*" && \
+			echo "Text: 		$(call get_label_text,$*)" && \
+			echo "Image Name: 	$(call get_label_image_name,$*)" && \
+			echo "Dockerfile: 	$(call get_label_dockerfile,$*)" && \
+			echo ""; \
 	fi
 
-python38_base-help:
-	@echo "$(PYTHON38_TEXT) Base Image Targets:"
+# Pattern rule for building only allowed base images
+%-build: validate-%
+	@[ ! -f $(call get_label_dockerfile,$*) ] && \
+		echo "Dockerfile not found" || \
+		$(call docker-build, $(call get_label_dockerfile,$*), $(call get_label_image_name,$*), .)
+
+%-rebuild:
+	@$(MAKE) --no-print-directory $*-build FORCE_REBUILD=1
+
+%-bash: %-build
+	@docker run --rm -it --entrypoint /bin/bash $(call get_label_image_name,$*)
+
+%-test: %-build
+	@docker run --rm -it $(call get_label_image_name,$*) /bin/bash -c "echo 'Testing $(call get_label_text,$*) base image...'"
+
+%-drop:
+	@if docker image inspect $(call get_label_image_name,$*) > /dev/null 2>&1; then \
+		docker rmi -f $(call get_label_image_name,$*); \
+	else \
+		echo "Image $(call get_label_image_name,$*) does not exist."; \
+	fi
+
+%-help:
+	@echo "\033[1;34m$(call get_label_text,$*)\033[0m Base Image Targets"
 	@echo ""
-	@echo "  \033[1;32mpython38_base-build\033[0m      Build the $(PYTHON38_TEXT) base Docker image"
-	@echo "  \033[1;32mpython38_base-rebuild\033[0m    Rebuild the $(PYTHON38_TEXT) base Docker image (no cache)"
-	@echo "  \033[1;32mpython38_base-bash\033[0m      Start a bash shell in the $(PYTHON38_TEXT) base Docker image"
+	@echo "  \033[1;32m$*-build\033[0m      Build the $(call get_label_text,$*) base Docker image"
+	@echo "  \033[1;32m$*-rebuild\033[0m    Rebuild the $(call get_label_text,$*) base Docker image (no cache)"
+	@echo "  \033[1;32m$*-bash\033[0m      Start a bash shell in the $(call get_label_text,$*) base Docker image"
 	@echo ""
 
-###############################################################################
-# Python 3.11 Base
-###############################################################################
 
-PYTHON311_LABEL := "Python 3.11"
-PYTHON311_BASE_IMAGE_NAME := nlpssc/python311-base:latest
-PYTHON311_BASE_DOCKERFILE := .docker_imgs/python311_base/Dockerfile.python311_base
+help-base-images:
+	@$(foreach img,$(defined_base_images),$(MAKE) --no-print-directory $(img)-help;)
 
-python311_base-build:
-	@[ ! -f $(PYTHON311_BASE_DOCKERFILE) ] && \
-		echo "Dockerfile not found" || \
-		$(call docker-build, $(PYTHON311_BASE_DOCKERFILE), $(PYTHON311_BASE_IMAGE_NAME), .)
+# $(PYTHON38_LABEL)-build:
+# 	@[ ! -f $(PYTHON38_BASE_DOCKERFILE) ] && \
+# 		echo "Dockerfile not found" || \
+# 		$(call docker-build, $(PYTHON38_BASE_DOCKERFILE), $(PYTHON38_BASE_IMAGE_NAME), .)
 
-python311_base-rebuild:
-	@$(MAKE) python311_base-build FORCE_REBUILD=1
+# $(PYTHON38_LABEL)-rebuild:
+# 	@$(MAKE) $(PYTHON38_LABEL)-build FORCE_REBUILD=1
 
-python311_base-bash: python311_base-build
-	@docker run --rm -it --entrypoint /bin/bash $(PYTHON311_BASE_IMAGE_NAME)
+# $(PYTHON38_LABEL)-bash: $(PYTHON38_LABEL)-build
+# 	@docker run --rm -it --entrypoint /bin/bash $(PYTHON38_BASE_IMAGE_NAME)
 
-python311_base-test: python311_base-build
-	@docker run --rm -it $(PYTHON311_BASE_IMAGE_NAME) /bin/bash -c "echo 'Testing Python 3.11 base image...'"
+# $(PYTHON38_LABEL)-test: $(PYTHON38_LABEL)-build
+# 	@docker run --rm -it $(PYTHON38_BASE_IMAGE_NAME) /bin/bash -c "echo 'Testing $(PYTHON38_TEXT) base image...'"
 
-python311_base-drop:
-	@if docker image inspect $(PYTHON311_BASE_IMAGE_NAME) > /dev/null 2>&1; then \
-		docker rmi -f $(PYTHON311_BASE_IMAGE_NAME); \
-	else \
-		echo "Image $(PYTHON311_BASE_IMAGE_NAME) does not exist."; \
-	fi
+# $(PYTHON38_LABEL)-drop:
+# 	@if docker image inspect $(PYTHON38_BASE_IMAGE_NAME) > /dev/null 2>&1; then \
+# 		docker rmi -f $(PYTHON38_BASE_IMAGE_NAME); \
+# 	else \
+# 		echo "Image $(PYTHON38_BASE_IMAGE_NAME) does not exist."; \
+# 	fi
+
+# $(PYTHON38_LABEL)-help:
+# 	@echo "$(PYTHON38_TEXT) Base Image Targets:"
+# 	@echo ""
+# 	@echo "  \033[1;32m$(PYTHON38_LABEL)-build\033[0m      Build the $(PYTHON38_TEXT) base Docker image"
+# 	@echo "  \033[1;32m$(PYTHON38_LABEL)-rebuild\033[0m    Rebuild the $(PYTHON38_TEXT) base Docker image (no cache)"
+# 	@echo "  \033[1;32m$(PYTHON38_LABEL)-bash\033[0m      Start a bash shell in the $(PYTHON38_TEXT) base Docker image"
+# 	@echo ""
+
+# ###############################################################################
+# # Python 3.11 Base
+# ###############################################################################
+
+# PYTHON311_LABEL := "Python 3.11"
+# PYTHON311_BASE_IMAGE_NAME := nlpssc/python311-base:latest
+# PYTHON311_BASE_DOCKERFILE := .docker_imgs/python311_base/Dockerfile.python311_base
+
+# python311_base-build:
+# 	@[ ! -f $(PYTHON311_BASE_DOCKERFILE) ] && \
+# 		echo "Dockerfile not found" || \
+# 		$(call docker-build, $(PYTHON311_BASE_DOCKERFILE), $(PYTHON311_BASE_IMAGE_NAME), .)
+
+# python311_base-rebuild:
+# 	@$(MAKE) python311_base-build FORCE_REBUILD=1
+
+# python311_base-bash: python311_base-build
+# 	@docker run --rm -it --entrypoint /bin/bash $(PYTHON311_BASE_IMAGE_NAME)
+
+# python311_base-test: python311_base-build
+# 	@docker run --rm -it $(PYTHON311_BASE_IMAGE_NAME) /bin/bash -c "echo 'Testing Python 3.11 base image...'"
+
+# python311_base-drop:
+# 	@if docker image inspect $(PYTHON311_BASE_IMAGE_NAME) > /dev/null 2>&1; then \
+# 		docker rmi -f $(PYTHON311_BASE_IMAGE_NAME); \
+# 	else \
+# 		echo "Image $(PYTHON311_BASE_IMAGE_NAME) does not exist."; \
+# 	fi
 
 
 
-###############################################################################
-# Python 3.12 Base
-###############################################################################
+# ###############################################################################
+# # Python 3.12 Base
+# ###############################################################################
 
-PYTHON312_LABEL := "Python 3.12"
-PYTHON312_BASE_IMAGE_NAME := nlpssc/python312-base:latest
-PYTHON312_BASE_DOCKERFILE := .docker_imgs/python312_base/Dockerfile.python312_base
+# PYTHON312_LABEL := "Python 3.12"
+# PYTHON312_BASE_IMAGE_NAME := nlpssc/python312-base:latest
+# PYTHON312_BASE_DOCKERFILE := .docker_imgs/python312_base/Dockerfile.python312_base
 
-python312_base-build:
-	@[ ! -f $(PYTHON312_BASE_DOCKERFILE) ] && \
-		echo "Dockerfile not found" || \
-		$(call docker-build, $(PYTHON312_BASE_DOCKERFILE), $(PYTHON312_BASE_IMAGE_NAME), .)
+# python312_base-build:
+# 	@[ ! -f $(PYTHON312_BASE_DOCKERFILE) ] && \
+# 		echo "Dockerfile not found" || \
+# 		$(call docker-build, $(PYTHON312_BASE_DOCKERFILE), $(PYTHON312_BASE_IMAGE_NAME), .)
 
-python312_base-rebuild:
-	@$(MAKE) python312_base-build FORCE_REBUILD=1
+# python312_base-rebuild:
+# 	@$(MAKE) python312_base-build FORCE_REBUILD=1
 
-python312_base-bash: python312_base-build
-	@docker run --rm -it --entrypoint /bin/bash $(PYTHON312_BASE_IMAGE_NAME)
+# python312_base-bash: python312_base-build
+# 	@docker run --rm -it --entrypoint /bin/bash $(PYTHON312_BASE_IMAGE_NAME)
 
-python312_base-test: python312_base-build
-	@docker run --rm -it $(PYTHON312_BASE_IMAGE_NAME) /bin/bash -c "echo 'Testing Python 3.12 base image...'"
+# python312_base-test: python312_base-build
+# 	@docker run --rm -it $(PYTHON312_BASE_IMAGE_NAME) /bin/bash -c "echo 'Testing Python 3.12 base image...'"
 
-python312_base-drop:
-	@if docker image inspect $(PYTHON312_BASE_IMAGE_NAME) > /dev/null 2>&1; then \
-		docker rmi -f $(PYTHON312_BASE_IMAGE_NAME); \
-	else \
-		echo "Image $(PYTHON312_BASE_IMAGE_NAME) does not exist."; \
-	fi
+# python312_base-drop:
+# 	@if docker image inspect $(PYTHON312_BASE_IMAGE_NAME) > /dev/null 2>&1; then \
+# 		docker rmi -f $(PYTHON312_BASE_IMAGE_NAME); \
+# 	else \
+# 		echo "Image $(PYTHON312_BASE_IMAGE_NAME) does not exist."; \
+# 	fi
 
-###############################################################################
-# MedSpacy QuickUMLS Processor Base
-###############################################################################
+# ###############################################################################
+# # MedSpacy QuickUMLS Processor Base
+# ###############################################################################
 
-MEDSPACY_QUICKUMLS_PROCESSOR_LABEL := "Medspacy-QuickUMLS Processor"
-MEDSPACY_QUICKUMLS_PROCESSOR_IMAGE_NAME := nlpssc/medspacy-quickumls-processor:latest
-MEDSPACY_QUICKUMLS_PROCESSOR_DOCKERFILE := .docker_imgs/medspacy_quickumls_processor/Dockerfile.medspacy_quickumls_processor
+# MEDSPACY_QUICKUMLS_PROCESSOR_LABEL := "Medspacy-QuickUMLS Processor"
+# MEDSPACY_QUICKUMLS_PROCESSOR_IMAGE_NAME := nlpssc/medspacy-quickumls-processor:latest
+# MEDSPACY_QUICKUMLS_PROCESSOR_DOCKERFILE := .docker_imgs/medspacy_quickumls_processor/Dockerfile.medspacy_quickumls_processor
 
-medspacy_quickumls_processor-build:
-	@[ ! -f $(MEDSPACY_QUICKUMLS_PROCESSOR_DOCKERFILE) ] && \
-		echo "Dockerfile not found" || \
-		$(call docker-build, $(MEDSPACY_QUICKUMLS_PROCESSOR_DOCKERFILE), $(MEDSPACY_QUICKUMLS_PROCESSOR_IMAGE_NAME), .)
+# medspacy_quickumls_processor-build:
+# 	@[ ! -f $(MEDSPACY_QUICKUMLS_PROCESSOR_DOCKERFILE) ] && \
+# 		echo "Dockerfile not found" || \
+# 		$(call docker-build, $(MEDSPACY_QUICKUMLS_PROCESSOR_DOCKERFILE), $(MEDSPACY_QUICKUMLS_PROCESSOR_IMAGE_NAME), .)
 
-medspacy_quickumls_processor-rebuild:
-	@$(MAKE) medspacy_quickumls_processor-build FORCE_REBUILD=1
+# medspacy_quickumls_processor-rebuild:
+# 	@$(MAKE) medspacy_quickumls_processor-build FORCE_REBUILD=1
 
-medspacy_quickumls_processor-bash: medspacy_quickumls_processor-build
-	@docker run --rm -it --entrypoint /bin/bash $(MEDSPACY_QUICKUMLS_PROCESSOR_IMAGE_NAME)
+# medspacy_quickumls_processor-bash: medspacy_quickumls_processor-build
+# 	@docker run --rm -it --entrypoint /bin/bash $(MEDSPACY_QUICKUMLS_PROCESSOR_IMAGE_NAME)
 
-medspacy_quickumls_processor-test: medspacy_quickumls_processor-build
-	@docker run --rm -it $(MEDSPACY_QUICKUMLS_PROCESSOR_IMAGE_NAME) /bin/bash -c "echo 'Testing Python 3.8 base image...'"
+# medspacy_quickumls_processor-test: medspacy_quickumls_processor-build
+# 	@docker run --rm -it $(MEDSPACY_QUICKUMLS_PROCESSOR_IMAGE_NAME) /bin/bash -c "echo 'Testing Python 3.8 base image...'"
 
-medspacy_quickumls_processor-drop:
-	@if docker image inspect $(MEDSPACY_QUICKUMLS_PROCESSOR_IMAGE_NAME) > /dev/null 2>&1; then \
-		docker rmi -f $(MEDSPACY_QUICKUMLS_PROCESSOR_IMAGE_NAME); \
-	else \
-		echo "Image $(MEDSPACY_QUICKUMLS_PROCESSOR_IMAGE_NAME) does not exist."; \
-	fi
+# medspacy_quickumls_processor-drop:
+# 	@if docker image inspect $(MEDSPACY_QUICKUMLS_PROCESSOR_IMAGE_NAME) > /dev/null 2>&1; then \
+# 		docker rmi -f $(MEDSPACY_QUICKUMLS_PROCESSOR_IMAGE_NAME); \
+# 	else \
+# 		echo "Image $(MEDSPACY_QUICKUMLS_PROCESSOR_IMAGE_NAME) does not exist."; \
+# 	fi
 	
+
+# base-drop-images: python38_base-drop python311_base-drop python312_base-drop medspacy_quickumls_processor-drop 
+
 endif
-
-base-drop-images: python38_base-drop python311_base-drop python312_base-drop medspacy_quickumls_processor-drop 
-
